a43c1475b242e4d566cdb13b0ec5874aa5e6eae9
Vanya Holovey <golovejvana9@gmail.com>
2025-08-14 21:19:38 +0300

    React Todo App Add and Delete

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useState } from 'react';
+import React from 'react';
 import classNames from 'classnames';
 import { Todo } from '../types/Todo';
 
