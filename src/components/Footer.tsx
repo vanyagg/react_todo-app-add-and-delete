b58fef5a05a07ae84756d81d3d8ff365da66a4f7
Vanya Holovey <golovejvana9@gmail.com>
2025-08-14 21:03:25 +0300

    React Todo App Add and Delete

---
+++ src/components/Footer.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import classNames from 'classnames';
+import { Todo } from '../types/Todo';
+
+type Props = {
+  todos: Todo[];
+  allTodos: Todo[];
+  currentSelect: string;
+  onSaveTodos: (todos: Todo[]) => void;
+  onSaveCurrentSelect: (filter: string) => void;
+  onClearCompleted: () => void;
+};
+
+export const Footer: React.FC<Props> = ({
+  todos,
+  allTodos,
+  currentSelect,
+  onSaveTodos,
+  onSaveCurrentSelect,
+  onClearCompleted,
+}) => {
+  const filterByStatus = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    const filter = event.currentTarget.textContent;
+
+    onSaveTodos(
+      allTodos.filter((todo: Todo) => {
+        if (filter === 'Active') {
+          return !todo.completed;
+        } else if (filter === 'Completed') {
+          return todo.completed;
+        }
+
+        return true;
+      }),
+    );
+
+    if (filter !== null) {
+      onSaveCurrentSelect(filter);
+    }
+  };
+
+  const completedTodos = () => {
+    return todos.some(todo => todo.completed);
+  };
+
+  const todosCounter = () => allTodos.filter(todo => !todo.completed).length;
+
+  return (
+    <footer className="todoapp__footer" data-cy="Footer">
+      <span className="todo-count" data-cy="TodosCounter">
+        {todosCounter()} items left
+      </span>
+
+      {/* Active link should have the 'selected' class */}
+      <nav className="filter" data-cy="Filter">
+        <a
+          href="#/"
+          className={classNames('filter__link', {
+            selected: currentSelect === 'All',
+          })}
+          data-cy="FilterLinkAll"
+          onClick={filterByStatus}
+        >
+          All
+        </a>
+
+        <a
+          href="#/active"
+          className={classNames('filter__link', {
+            selected: currentSelect === 'Active',
+          })}
+          data-cy="FilterLinkActive"
+          onClick={filterByStatus}
+        >
+          Active
+        </a>
+
+        <a
+          href="#/completed"
+          className={classNames('filter__link', {
+            selected: currentSelect === 'Completed',
+          })}
+          data-cy="FilterLinkCompleted"
+          onClick={filterByStatus}
+        >
+          Completed
+        </a>
+      </nav>
+
+      {/* this button should be disabled if there are no completed todos */}
+      <button
+        type="button"
+        className="todoapp__clear-completed"
+        data-cy="ClearCompletedButton"
+        onClick={() => onClearCompleted()}
+        disabled={!completedTodos()}
+      >
+        Clear completed
+      </button>
+    </footer>
+  );
+};
